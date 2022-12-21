81b8ffea5eb9a18c3da6ee74f0123ea32ff9ce85
Tim Rainer <tim.rainer04@gmail.com>
2022-12-21 13:58:11 +0100

    remove comments

--- detection/detex.py
+++ detection/detex.py
@@ -13,11 +13,9 @@ mpHands = mp.solutions.hands
 hands = mpHands.Hands(max_num_hands=1, min_detection_confidence=0.7)
 mpDraw = mp.solutions.drawing_utils
 
-# Load the gesture recognizer model
-# model = load_model('mp_hand_gesture')
+
 model = load_model('model.hdf5')
-# Load class names
-# f = open('gesture.names', 'r')
+
 classNames = {
     0: "Flat Hand",
     1: "O-Kay",
@@ -25,17 +23,16 @@ classNames = {
     3: "Three Fingers"
 }
 
-# Initialize the webcam
+
 cap = cv2.VideoCapture(0)
 i = 0
 while True:
-    # Read each frame from the webcam
     _, frame = cap.read()
 
 
     image_width, image_height = frame.shape[1], frame.shape[0]
 
-    # Flip the frame vertically
+
     frame = cv2.flip(frame, 1)
 
     framergb = frame
@@ -44,14 +41,13 @@ while True:
     
     className = ''
 
-    # post process the result
+
     if result.multi_hand_landmarks:
 
         landmarks = []
         for handslms in result.multi_hand_landmarks:
             i = 0
             for lm in handslms.landmark:
-                # print(id, lm)
                 lmx = int(lm.x * image_width)
                 lmy = int(lm.y * image_height)
                 if i == 0:
@@ -63,7 +59,6 @@ while True:
                     lmx = lmx - temp_x
                     lmy = lmy - temp_y
 
-                # landmarks.append([lmx, lmy])
                 landmarks.append(lm.x)
                 landmarks.append(lm.y)
 
@@ -85,20 +80,7 @@ while True:
             
             print(classID)
             print(className)
-            # emoji_path = "emojis/" + str(classID) + ".png"
-
-            # overlay = cv2.imread(emoji_path)
-            # if classID == 0:
-            #     overlay = cv2.resize(overlay, (150, 150))
-            # else:
-            #     overlay = cv2.resize(overlay, (100, 100))
-            # h, w = overlay.shape[:2]
-            # shapes = np.zeros_like(framergb, np.uint8)
-            # shapes[0:h, 0:w] = overlay
-            # alpha = 0.8
-            # mask = shapes.astype(bool)
-            # framergb[mask] = cv2.addWeighted(shapes, alpha, shapes, 1 - alpha, 0)[mask]
-    # Show the final output
+            
     cv2.putText(framergb, className, (450, 220), cv2.FONT_HERSHEY_SIMPLEX,
                    1, (0, 0, 255), 2, cv2.LINE_AA)
     cv2.imshow("Output", framergb)
@@ -106,7 +88,6 @@ while True:
     if cv2.waitKey(1) == ord('q'):
         break
 
-# release the webcam and destroy all active windows
 cap.release()
 
 cv2.destroyAllWindows()
